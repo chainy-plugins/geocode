65f27d9e4cedb28923ce27f24fb8a91a24b01c30
Benjamin Lupton <b@lupton.cc>
2014-05-21 19:49:18 +1000

    v0.1.0. Abstracted out.
    - v0.1.0 May 21, 2014
    	- Abstracted out of core

---
+++ test/geocode-test.js
@@ -0,0 +1,22 @@
+(function(){
+	// Import
+	var expect = require('chai').expect,
+		joe = require('joe')
+
+	// Test
+	joe.describe('geocode plugin', function(describe,it){
+		var Chainy = require('chainy-core').subclass().require('set').addExtension('geocode', require('../'))
+
+		it("should work", function(next){
+			Chainy.create()
+				.set('Sydney, Australia')
+				.geocode()
+				.done(function(err, result){
+					debugger;
+					if (err)  return next(err)
+					expect(result, 'coordinates match').to.deep.equal([151.059617, -33.838197])
+					return next()
+				})
+		})
+	})
+})()
